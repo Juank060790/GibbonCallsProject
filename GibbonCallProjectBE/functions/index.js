55234c0ce315f9d5f90d5d88df6dabd13087c34e
JuanCa <juank060790@gmail.com>
2021-04-05 17:00:24 +0700

    Client Server

--- GibbonCallProjectBE/functions/index.js
+++
@@ -1,9 +0,0 @@
-const cors = require("cors");
-const app = require("express")();
-app.use(cors());
-var indexRouter = require("./routes/index");
-const functions = require("firebase-functions");
-
-app.use("/", indexRouter);
-
-exports.api = functions.https.onRequest(app);
