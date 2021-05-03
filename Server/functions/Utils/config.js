b440683145548e0b0057a82fbba99f8b3ec9b35b
JuanCa <juank060790@gmail.com>
2021-05-03 18:01:00 +0700

    link test storage

--- Server/functions/Utils/config.js
+++ Server/functions/Utils/config.js
@@ -1,43 +1,10 @@
-// module.exports = {
-//   apiKey: process.env.APIKEY,
-//   authDomain: process.env.AUTHDOMAIN,
-//   databaseURL: process.env.DATABASEURL,
-//   projectId: process.env.PROJECTID,
-//   storageBucket: process.env.STORAGEBUCKET,
-//   messagingSenderId: process.env.MESSAGINGSENDERID,
-//   appId: process.env.APPID,
-//   measurementId: process.env.MESUREMENTID,
-// };
-
-// module.exports = {
-//   apiKey: "AIzaSyDEaLTKqC-NIWv4iI0co2izSPPzDRhO4jI",
-//   authDomain: "test-gibbonproject.firebaseapp.com",
-//   databaseURL:
-//     "https://test-gibbonproject-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "test-gibbonproject",
-//   storageBucket: "test-gibbonproject.appspot.com",
-//   messagingSenderId: "816998532445",
-//   appId: "1:816998532445:web:c7932a7a9cceee2f4e54b4",
-//   measurementId: "G-JY1FG9FGZV",
-// };
-
-// module.exports = {
-//   apiKey: "AIzaSyC8TEMZYOeF1gFAbVDwiqqE0h8GfSL4kCk",
-//   authDomain: "coderschool-project-gibbon.firebaseapp.com",
-//   projectId: "coderschool-project-gibbon",
-//   storageBucket: "coderschool-project-gibbon.appspot.com",
-//   messagingSenderId: "591790114188",
-//   appId: "1:591790114188:web:288819c209bcabe9efc2a9",
-//   measurementId: "G-6X65GQ4HSG",
-// };
-
 module.exports = {
-  apiKey: "AIzaSyC8TEMZYOeF1gFAbVDwiqqE0h8GfSL4kCk",
-  authDomain: "coderschool-project-gibbon.firebaseapp.com",
-  databaseURL: "https://coderschool-project-gibbon-default-rtdb.firebaseio.com",
-  projectId: "coderschool-project-gibbon",
-  storageBucket: "coderschool-project-gibbon.appspot.com",
-  messagingSenderId: "591790114188",
-  appId: "1:591790114188:web:288819c209bcabe9efc2a9",
-  measurementId: "G-6X65GQ4HSG",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MESUREMENTID,
 };
