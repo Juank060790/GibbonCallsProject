e66dd1b057d4c9257d00d61dc2675291211e3fbe
JuanCa <juank060790@gmail.com>
2021-05-26 11:32:27 +0700

    add regions updated it

--- Client/src/Firebase/firebase.js
+++ Client/src/Firebase/firebase.js
@@ -3,16 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC8TEMZYOeF1gFAbVDwiqqE0h8GfSL4kCk",
-  authDomain: "coderschool-project-gibbon.firebaseapp.com",
-  databaseURL: "https://coderschool-project-gibbon-default-rtdb.firebaseio.com",
-  projectId: "coderschool-project-gibbon",
-  storageBucket: "coderschool-project-gibbon.appspot.com",
-  messagingSenderId: "591790114188",
-  appId: "1:591790114188:web:288819c209bcabe9efc2a9",
-  measurementId: "G-6X65GQ4HSG",
-};
 
 // module.exports = {
 //   apiKey: process.env.APIKEY,
