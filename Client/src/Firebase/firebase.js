68727e60b10852e692188c72213463dede558c7e
JuanCa <juank060790@gmail.com>
2021-06-01 15:53:31 +0700

    #

--- Client/src/Firebase/firebase.js
+++ Client/src/Firebase/firebase.js
@@ -4,14 +4,16 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-//   apiKey: process.env.APIKEY,
-//   authDomain: process.env.AUTHDOMAIN,
-//   databaseURL: process.env.DATABASEURL,
-//   projectId: process.env.PROJECTID,
-//   storageBucket: process.env.STORAGEBUCKET,
-//   messagingSenderId: process.env.MESSAGINGSENDERID,
-//   appId: process.env.APPID,
-//   measurementId: process.env.MESUREMENTID,
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MESUREMENTID,
+};
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
 
